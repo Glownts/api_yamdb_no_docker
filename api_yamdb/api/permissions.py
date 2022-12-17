8fa0bcf234e5f87837cd70ad5489e308c0e17735
Андрей Легкий <legkiy.andrey@bk.ru>
2022-12-17 20:19:45 +0300

    Revision of serializers and permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,29 +1,60 @@
-'''
-Разрешения приложения api.
-'''
-
 from rest_framework import permissions
 
 
 class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return True
+        if request.user.is_authenticated:
+            return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin'
+                or request.user.is_superuser
+            )
+        return request.method in permissions.SAFE_METHODS
 
     def has_object_permission(self, request, view, obj):
-        return True
+        if request.user.is_authenticated:
+            return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin'
+                or request.user.is_superuser
+            )
+        return request.method in permissions.SAFE_METHODS
 
 
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return True
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return True
-
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.role == 'admin'
+        )
 
 class MeOrAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return True
+        if request.user.is_authenticated:
+            return True
+        return False
+    def has_object_permission(self, request, view, obj):
+        if obj.author == request.user:
+            return True
+        return False
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return (
+                request.user.role == 'admin' or request.user.is_superuser)
+        return False
 
     def has_object_permission(self, request, view, obj):
-        return True
+        if request.user.is_authenticated:
+            return (request.user.role == 'admin' or request.user.is_superuser)
+        return False
+
